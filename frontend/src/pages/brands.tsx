99b47e861f2d2d9e1ce8840ef030fc2b23b8e850
Hassan Ali <hassan.ali.cs7@gmail.com>
2025-04-24 14:41:23 +0500

    frontend - apis integrated

--- frontend/src/pages/brands.tsx
+++ frontend/src/pages/brands.tsx
@@ -13,6 +13,47 @@ interface Brand {
 
 function Brands() {
   const [brands, setBrands] = useState<Brand[]>([])
+  const [amount, setAmount] = useState(0);
+
+  const handleAmountChange = (event: any) => {
+    setAmount(event.target.value);
+  };
+
+  const handleAmountSubmit = (event: any, brandId: number) => {
+    event.preventDefault();
+
+    api.put(`brands/${brandId}/record_spend/`, {
+      amount
+    }).then(response => {
+        alert(`Amount Spent`);
+        setAmount(0)
+      })
+      .catch(error => {
+        alert('Error handling Amount Submit');
+      });
+  };
+
+  const handleResetDaily = (event: any, brandId: number) => {
+    event.preventDefault();
+    api.put(`brands/${brandId}/reset_daily/`)
+      .then(response => {
+        alert(`Daily Reset Successfull`);
+      })
+      .catch(error => {
+        alert('Error resetting daily');
+      });
+  };
+
+  const handleResetMonthly = (event: any, brandId: number) => {
+    event.preventDefault();
+    api.put(`brands/${brandId}/reset_monthly/`)
+      .then(response => {
+        alert(`Monthly Reset Successfull`);
+      })
+      .catch(error => {
+        alert('Error resetting monthly');
+      });
+  };
 
   useEffect(() => {
     api.get('brands/')
@@ -41,14 +82,14 @@ function Brands() {
             <tr key={index}>
               <td className="border border-gray-300">{brand.name}</td>
               <td className="border border-gray-300">
-                <input type="number" id="spend_amount" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="50" />
-                <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Spend</button>
+                <input type="number" id="spend_amount" value={amount} onChange={handleAmountChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="50" />
+                <button type="button" onClick={(e) => handleAmountSubmit(e, brand.id)} className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Spend</button>
               </td>
               <td className="border border-gray-300">
-                <button type="button" className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Reset Daily</button>
+                <button type="button" onClick={(e) => handleResetDaily(e, brand.id)} className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Reset Daily</button>
               </td>
               <td className="border border-gray-300">
-                <button type="button" className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Reset Monthly</button>
+                <button type="button" onClick={(e) => handleResetMonthly(e, brand.id)} className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Reset Monthly</button>
               </td>
               <td className="border border-gray-300"><Link to={`/brands/${brand.id}/campaigns`} className="text-blue-500">View Campaigns</Link></td>
             </tr>
